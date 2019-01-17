bddb7a35099b8114e2fa57ee527132b83ed675dd
Martin Stamenkov <bul2013@abv.bg>
2019-01-17 19:18:17 +0200

    semi-final version

--- final_project_doc/app/controllers/documents_controller.rb
+++ final_project_doc/app/controllers/documents_controller.rb
@@ -2,11 +2,12 @@ class DocumentsController < ApplicationController
   	def create
   		document = String(params[:value]);
   		Document.create(text:document);
+  		redirect_to documents_path;
   	end
   	
   	
   	def list
-  		Document.all;
+  		@lists = Document.all;
   	end
   	
   	def show
