41abffa2484d04bb353ec6e726e122bbb0fc659e
Martin Stamenkov <bul2013@abv.bg>
2019-01-25 10:48:36 +0200

    dropdown fix v.2

--- Events_project/app/models/event.rb
+++ Events_project/app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
-	
+	validates :name, presence: true, length: {minimum: 8 }
+	validates :location, presence: true
+	validates :description, presence: true
+	validates :time, presence: true
 	belongs_to :people
 end
